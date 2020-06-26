28e3792502c6d86cdfd05582361a0d1e49580e18
jschrbr <schrbrr@gmail.com>
2020-06-27 04:05:07 +1000

    :(

--- config/configDB.ts
+++ config/configDB.ts
@@ -2,11 +2,11 @@ import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 import { dso } from "https://deno.land/x/dso@v1.0.0/mod.ts";
 
-const { DB_URL, JAWSDB_URL } = config()
+const env = config()
 
 const url_patt = /^mysql:\/\/([^:@]+):?([^@]+)?@([^:]+):([^\/]+)\/(.+)$/i
-const db_url = DB_URL || JAWSDB_URL
-console.log(db_url)
+console.log(env)
+const db_url = env.DB_URL || env.JAWSDB_URL
 const res: any = db_url.match(url_patt)
 const [_, username, pass, hostname, sql_port, db] = res
 const password = pass || ""
