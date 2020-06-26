df8a2d8d9fe6573cd3ad47e9f69bd796a2008699
jschrbr <schrbrr@gmail.com>
2020-06-27 04:24:32 +1000

    Better .env read

--- config/configDB.ts
+++ config/configDB.ts
@@ -2,10 +2,10 @@ import "https://deno.land/x/dotenv/load.ts";
 
 import { dso } from "https://deno.land/x/dso@v1.0.0/mod.ts";
 
-const env = Deno.env.toObject()
+const { DB_URL, JAWSDB_URL } = Deno.env.toObject()
 
 const url_patt = /^mysql:\/\/([^:@]+):?([^@]+)?@([^:]+):([^\/]+)\/(.+)$/i
-const db_url = env.DB_URL || env.JAWSDB_URL
+const db_url = DB_URL || JAWSDB_URL
 const res: any = db_url.match(url_patt)
 const [_, username, pass, hostname, sql_port, db] = res
 const password = pass || ""
