35cb0c2af414313cccd0bfe3f3bd5c3b56ba61bf
jschrbr <schrbrr@gmail.com>
2020-07-01 01:40:28 +1000

    Updated create script. still needs work

--- scripts/create.sh
+++ scripts/create.sh
@@ -5,5 +5,5 @@ npm i -g heroku
 fi
 heroku login
 heroku create example
-heroku buildpack:set https://github.com/chibat/heroku-buildpack-deno.git
+heroku buildpacks:set https://github.com/chibat/heroku-buildpack-deno.git
 heroku addons:create jawsdb
